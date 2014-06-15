597b452f84eef0c3fe7ce8001b8febdf58d9207c
Amelia Fannin <ameliafannin@gmail.com>
2014-06-14 21:51:38 -0400

    refactored

--- euler-002.rb
+++ euler-002.rb
@@ -7,39 +7,23 @@
 #By considering the terms in the Fibonacci sequence 
 #whose values do not exceed four million, 
 #find the sum of the even-valued terms.
-#4613732
 
-class FibSequence
+# answer: 4613732
+# time: .106s
 
-	def initialize(r, first, second)
-		@range_limit = r
-		@num1 = first
-		@num2 = second
-		@values = [first, second]
-		@val = 0
-		@total = 0
+def fib_sequence
+	a = 1
+	b = 0
+	sum = 0
 
-		fib_loop
+	while a < 4000000 do 
+		fib = a + b
+		sum += fib if fib.even?
+		b = a
+		a = fib
 	end
 
-	def fib_loop
-		while (@num1 + @num2 < @range_limit) do
-			@val = @num1 + @num2
-			@values<<@val
-			@num1 = @num2
-			@num2 = @val
-    end	
-    sum_evens
-	end
-	
-	def sum_evens
-		@values.each do |n|
-      if n % 2 == 0
-		  @total = @total + n
-      end
-		end
-    puts @total
-	end	
+	sum
 end
 
-FibSequence.new(4000000, 1, 2)
+puts fib_sequence
