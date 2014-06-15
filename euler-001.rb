8e62c2da99714b7fc1c31f4424678f539e98e7ba
Amelia Fannin <ameliafannin@gmail.com>
2014-06-14 21:44:22 -0400

    refactored

--- euler-001.rb
+++ euler-001.rb
@@ -1,39 +1,17 @@
-#If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+#If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+ # The sum of these multiples is 23.
 
 #Find the sum of all the multiples of 3 or 5 below 1000.
 
 #answer = 233168
-class Summer
-    
-  def initialize(r)
-    @range_limit = r
-    @multiples = []
-    @num = 0
-    @total = 0
+# time = .087s
 
-    run_loop
+def multiples
+  sum = 0
+  1.upto(999) do |i|
+    sum += i if i % 3 == 0 or i % 5 == 0
   end
-
-  def sum  
-    @multiples.each do |m|
-    @total = @total + m
-    end
-  end
-
-  def get_multiples
-    if (@num % 3 == 0 || @num % 5 == 0)
-      @multiples<<@num
-    end
-  end
-
-  def run_loop
-    while (@num < @range_limit) do 
-      get_multiples
-      @num += 1
-    end
-    sum
-    puts @total
-  end
+  sum
 end
 
-Summer.new(1000)
+puts multiples
