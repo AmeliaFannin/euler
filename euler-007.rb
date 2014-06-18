4106ab0f738667d1aa706d81c36661f99c23d42f
Amelia Fannin <ameliafannin@gmail.com>
2014-06-18 13:09:39 -0400

    refactored

--- euler-007.rb
+++ euler-007.rb
@@ -1,15 +1,14 @@
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, 
 # we can see that the 6th prime is 13.
 
-# What is the 10 001st prime number? 104743
+# What is the 10 001st prime number? 
+# answer: 104743
+# time: .118s
 
 require 'prime'
 
 def last_prime i
-  primes = []
-  primes = Prime.first i
-  puts "Answer is #{primes.last}"
+  Prime.first(i).last
 end
 
-
-last_prime 10001
+puts last_prime 10001
