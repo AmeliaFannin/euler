5416673b2aeed69184d497bcb1e69d94f9743ea7
Amelia Fannin <ameliafannin@gmail.com>
2014-06-25 14:15:41 -0400

    refactored

--- euler-010.rb
+++ euler-010.rb
@@ -2,26 +2,20 @@
 
 # Find the sum of all the primes below two million.
 
+# answer: 142913828922
+# time: .4s
+
 require 'prime'
 
-# @method prime_sum
-# @desc Finds the sum...
-# @returns integer
+def prime_sum
+  sum = 0
 
-def prime_sum range
-  range = range
-  sum = 2
-  i = 3
-
-  while i < range do 
-    if i.prime?
-      sum = sum + i
-    end
-
-    i += 2
+  Prime.each do |prime|
+    break if prime > 2000000
+    sum += prime 
   end
 
   sum
 end
 
-puts prime_sum 100
+puts prime_sum
