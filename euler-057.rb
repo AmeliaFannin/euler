86d95b5b8c111a1a2da1a7e9aecf9476b0583be7
Amelia Fannin <ameliafannin@gmail.com>
2014-09-24 12:29:55 -0400

    #57

---
+++ euler-057.rb
@@ -0,0 +1,45 @@
+# Square root convergents
+# Problem 57
+# It is possible to show that the square root of two can be 
+# expressed as an infinite continued fraction.
+
+# √ 2 = 1 + 1/(2 + 1/(2 + 1/(2 + ... ))) = 1.414213...
+
+# By expanding this for the first four iterations, we get:
+
+# 1 + 1/2 = 3/2 = 1.5
+# 1 + 1/(2 + 1/2) = 7/5 = 1.4
+# 1 + 1/(2 + 1/(2 + 1/2)) = 17/12 = 1.41666...
+# 1 + 1/(2 + 1/(2 + 1/(2 + 1/2))) = 41/29 = 1.41379...
+
+# The next three expansions are 99/70, 239/169, and 577/408, 
+# but the eighth expansion, 1393/985, is the first example where the number of digits 
+# in the numerator exceeds the number of digits in the denominator.
+
+# In the first one-thousand expansions, 
+# how many fractions contain a numerator with more digits than denominator?
+
+#answer: 153
+# time: .090s
+
+def convergents
+  count = 0
+
+  p = 3
+  q = 2
+
+  1000.times do 
+    count += 1 if p.to_s.length > q.to_s.length
+      
+    next_p = p + 2 * q
+    next_q = p + q
+    
+    p = next_p
+    q = next_q
+  end
+
+  count
+end
+
+puts convergents
+
