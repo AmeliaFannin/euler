999c8a8367145ce4b3c7bd4ac9fccac6b706ff06
Amelia Fannin <ameliafannin@gmail.com>
2014-09-24 12:35:28 -0400

    refactored

--- python/euler-001.py
+++ python/euler-001.py
@@ -3,15 +3,18 @@
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, 
 # we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
-# Find the sum of all the multiples of 3 or 5 below 1000.
+# Find the sum of all the multiples of 3 or 5 below 1000
+# answer = 233168
+# time = .024s
+
 
 def multiples(limit):
-    sum = 0
+    total = 0
+
     for x in range(limit):
-        if x % 3 == 0:
-            sum = sum + x
-        elif x % 5 == 0:
-            sum = sum + x
-    print sum
+        if x % 3 == 0 or x % 5 == 0:
+            total += x
+            
+    print total
 
 multiples(1000)
